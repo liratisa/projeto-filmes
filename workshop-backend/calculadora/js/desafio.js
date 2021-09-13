0b4a9cd12655001d4c3df768f00361ca4456f14d
Isabela <83988818+liratisa@users.noreply.github.com>
2021-09-13 18:03:11 -0300

    Delete workshop-backend directory

--- workshop-backend/calculadora/js/desafio.js
+++
@@ -1,7 +0,0 @@
-var inputValorHora = document.querySelector("#valor-hora");
-var inputHorasProjeto = document.querySelector("#horas-projeto");
-
-function calcular(){
-  var calculo = (inputValorHora.valueAsNumber * inputHorasProjeto.valueAsNumber).toFixed(2);
-  alert("O valor total do seu projeto é igual a R$" + calculo);
-}
--- workshop-backend/exemplo/script.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("h1") //querySelector não entende número
-h1.textContent //converte em texto comum sem as tags
